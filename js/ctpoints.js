724d65a402d318f700ea0aaefa3b02885f8a5f38
arpan0317 <arpan0317@yahoo.com>
2020-04-03 20:35:39 -0400

    added files

---
+++ js/ctpoints.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("ref_button_rheum");
+var span = document.getElementsByClassName("close")[0];
+var scroll = document.querySelector(".content");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+$(document).ready(function(){
+	init();
+});
+
+
+
+
+function init(){
+		
+}
+
